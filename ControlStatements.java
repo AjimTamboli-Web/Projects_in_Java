19591cb2638894c40fbf3c76d96a568669b8a7b3
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-19 19:28:17 +0530

    Using loops

---
+++ ControlStatements.java
@@ -0,0 +1,14 @@
+package projects_java;
+
+public class ControlStatements {
+
+	public static void main(String[] args) {
+
+	// In this project we are going to implements scenarios using all loops.
+		
+		
+
+
+	}
+
+}
